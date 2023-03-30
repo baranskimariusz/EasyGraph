752829b310b978b90726632943c86815119ce06f
baranskimariusz <mariusz.baranski@icloud.com>
2023-03-30 18:52:56 +0200

    Added descriptive statistics function

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import pandas as pd
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
-from matplotlib.figure import Figure
-import tkinter as tk
-from tkinter import filedialog
-
+import matplotlib; matplotlib.use('TkAgg'); import matplotlib.pyplot as plt; from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk); from matplotlib.figure import Figure
+import tkinter as tk; from tkinter import filedialog; from tkinter import *
+import sklearn as sk
 class EasyGraphApp(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -18,13 +13,15 @@ class EasyGraphApp(tk.Tk):
         container = tk.Frame(self)
         container.pack()
 
-        self.button1 = tk.Button(container, text='Close', width=20, command=self.destroy)
+        self.button1 = tk.Button(container, text='Close', width=18, command=self.destroy)
         self.button1.pack(side="right", padx=10, pady=10)
-        self.button2 = tk.Button(container, text="Load data", width=20, command=self.load_csv)
+        self.button2 = tk.Button(container, text="Load data", width=18, command=self.load_csv)
         self.button2.pack(side="left", padx=10, pady=10)
-        self.plot_button = tk.Button(container, text="Plot", width=20, command=self.plot_graph)
-        self.plot_button.pack(padx=10,pady=10)
+        self.plot_button = tk.Button(container, text="Plot", width=18, command=self.plot_graph)
+        self.plot_button.pack(side="left", padx=10,pady=10)
         self.data = None
+        self.stats_button = tk.Button(container, text="Show statistics", width=18, command=self.show_stats)
+        self.stats_button.pack(side="left", padx=10,pady=10)
     
     def load_csv(self):
         file_path = filedialog.askopenfilename(filetypes=[("XLSX files", "*.xlsx")])
@@ -53,6 +50,18 @@ class EasyGraphApp(tk.Tk):
             axes.legend()
         else:
             tk.messagebox.showerror("Error", "No data has been loaded.")
+
+    def show_stats(self):
+        if self.data is not None:
+            stats = self.data.describe()
+            popup = Toplevel()
+            popup.grab_set()
+            text = Text(popup, bd=5, height=10, width=50, bg='black')
+            text.pack()
+            text.insert(tk.END, stats)
+        else:
+            tk.messagebox.showerror("Error", "No data has been loaded.")
+
         
 if __name__ == "__main__":
     app = EasyGraphApp()
